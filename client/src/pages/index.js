9169145c6447d0bc239db49449b35f6b1bf42e5a
woogie3 <k333wook@gmail.com>
2020-01-02 15:35:58 +0900

    delete2
    delete2

--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Registoration } from './Registoration';
-export { default as Management } from './Management';
-export { default as Analysis } from './Analysis';
-export { default as QnA } from './QnA';
-export { default as ReviewManagement} from './ReviewManagement'
