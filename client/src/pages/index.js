e523de0d937a63bf11eda13abfd78c1652a68741
woogie03 <k333wook@gmail.com>
2020-01-02 17:19:55 +0900

    drop
    drop

--- client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as ManagementMenu } from './ManagementMenu';
-export { default as Analysis } from './Analysis';
-export { default as ShowCheck } from './ShowCheck';
-export { default as ReviewManagement} from './ReviewManagement';
-export { default as TheaterManagement} from './TheaterManagement';
-export { default as QnA } from './QnA';
-export { default as CeoMain } from './CeoMain';
